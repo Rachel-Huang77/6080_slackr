026b4fc637dd87549c84e7897fbe20256de8323e
Rachel Huang <rachel_ruoqi@outlook.com>
2025-10-24 19:54:15 +1100

    Delete src directory

--- src/main.js
+++
@@ -1,5 +0,0 @@
-// Main entry point for 6080_slackr application
-
-console.log('Welcome to 6080_slackr!');
-
-// Your code here
--- src/utils.js
+++
@@ -1,3 +0,0 @@
-// Utility functions for 6080_slackr
-
-// Add your utility functions here
